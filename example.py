07710473f863d79639a6edb721b08dfd0fe04951
Yam Peleg <ypeleg@github.com>
2019-08-21 18:02:27 +0300

    hunga bunga

--- example.py
+++ example.py
@@ -37,7 +37,6 @@ print(mdl.predict(X_c))
 X, y = random.choice(((X_c, y_c), (X_r, y_r)))
 mdl = HungaBungaZeroKnowledge()
 mdl.fit(X, y)
-print(mdl.predict(X))
-print(mdl.problem_type)
+print(mdl.predict(X), mdl.problem_type)
 
 #        <3 Dean, this is 4 U <3
